e7af82251044747b445d6e06965561f2658693a6
Berk <berkkarakas98@hotmail.com>
2023-09-23 22:10:56 +0300

    Add responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,27 @@
+import { Grid, GridItem, Show } from "@chakra-ui/react"
+import './App.css'
+
 function App() {
-  return 0
-}
+  return (
+  <Grid templateAreas={{
+    base:'"nav" "main" "footer"',
+    lg:`"nav nav" "aside main" "footer footer"`,
+  }}>
+    <GridItem area="nav" className="nav">
+      Nav
+    </GridItem>
+    <Show above="lg">
+      <GridItem area="aside" className="aside">
+        Aside
+      </GridItem>
+    </Show>
+    <GridItem area="main" className="main" >
+      Main
+    </GridItem>
+    <GridItem area="footer" className="footer">
+      Footer
+    </GridItem>
+  </Grid>
+)}
     
 export default App
